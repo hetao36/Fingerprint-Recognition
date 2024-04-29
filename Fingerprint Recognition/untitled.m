1d343fafc927dd096baf352808861cc48a111bb1
lizl <hetao9420@gmail.com>
2024-04-29 23:28:43 +0800

    Update untitled.m

--- Fingerprint Recognition/untitled.m
+++ Fingerprint Recognition/untitled.m
@@ -1,30 +1,5 @@
 function varargout = untitled(varargin)
-% UNTITLED MATLAB code for untitled.fig
-%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
-%      singleton*.
-%
-%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
-%      the existing singleton*.
-%
-%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UNTITLED.M with the given input arguments.
-%
-%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
- 
-% Edit the above text to modify the response to help untitled
- 
-% Last Modified by GUIDE v2.5 06-May-2017 13:58:51
- 
-% Begin initialization code - DO NOT EDIT
+
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
